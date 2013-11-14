cfe8924f254840cecde356231dbaf49e83d7326a
Bruno <Bruno@Bruno-Laptop>
2013-11-14 15:10:02 +0100

    zoekfunctie

--- src/java/info/toegepaste/www/models/Land.java
+++ src/java/info/toegepaste/www/models/Land.java
@@ -30,7 +30,7 @@ import javax.validation.constraints.NotNull;
 @Entity
 @Table(name = "land")
 @NamedQueries({
-    @NamedQuery(name = "Land.getAll", query = "SELECT l FROM Land l")
+    @NamedQuery(name = "Land.getAll", query = "SELECT l FROM Land l order by l.naam")
     
 })
 public class Land implements Serializable {
